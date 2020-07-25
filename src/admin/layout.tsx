0b51e963df72667cf2944636afb639c24a5899de
Mohammed khmalech <m.khmalech@gmail.com>
2020-07-25 21:40:20 +0100

    dont show dock menu

--- src/admin/layout.tsx
+++ src/admin/layout.tsx
@@ -18,7 +18,7 @@ export const AdminLayout = () => {
     const {username} = useSelector(({auth} : LaboFesState)=>auth.login) || undefined
 
     // close/open sideMenu
-    const [closeSideMenu, setcloseSideMenu] = React.useState(true)
+    const [closeSideMenu, setcloseSideMenu] = React.useState(false)
 
     // check if user connected
     const {isAuth} = useSelector((state: LaboFesState) => state.auth.login);
